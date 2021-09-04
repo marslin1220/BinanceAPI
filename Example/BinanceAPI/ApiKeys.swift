bb90b2ed29379ca594d163677fe976e24c898499
Cheng Lung Lin <marslin@gmail.com>
2021-09-04 11:07:11 +0800

    Add a struct to store API keys

---
+++ Example/BinanceAPI/ApiKeys.swift
@@ -0,0 +1,14 @@
+//
+//  ApiKeys.swift
+//  BinanceAPI_Example
+//
+//  Created by Lin Cheng Lung on 2021/9/4.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+struct ApiKeys {
+    let apiKey: String = "replace_me"
+    let secretKey: String = "replace_me"
+}

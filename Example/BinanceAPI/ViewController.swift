74f4d8a4088250e7cf37338073ac84cedf1096d6
Cheng Lung Lin <marslin@gmail.com>
2021-09-04 11:21:03 +0800

    Update the BinanceSavingService usage example

--- Example/BinanceAPI/ViewController.swift
+++ Example/BinanceAPI/ViewController.swift
@@ -6,13 +6,24 @@
 //  Copyright (c) 2021 Cheng Lung Lin. All rights reserved.
 //
 
+import BinanceAPI
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let apiKeys = ApiKeys()
+        let service = BinanceSavingService(apiKey: apiKeys.apiKey, secretKey: apiKeys.secretKey)
+        service.getProductList { result in
+            switch result {
+            case let .success(products):
+                print(products)
+            case let .failure(error):
+                print(error)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {

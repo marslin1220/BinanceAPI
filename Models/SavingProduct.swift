a8942ef976696bd163638cde0b4c4c66a1578618
Cheng Lung Lin <marslin@gmail.com>
2022-02-03 15:11:26 +0800

    Update the API: Flexible Product List (USER_DATA)
    * Remove the absent property: dailyInterestPerThousand
    * Add the additional property: tierAnnualInterestRate

--- Models/SavingProduct.swift
+++ Models/SavingProduct.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
+// https://binance-docs.github.io/apidocs/spot/en/#get-flexible-product-list-user_data
 public struct SavingProduct: Codable {
     let asset: String
     let avgAnnualInterestRate: Decimal?
     let canPurchase: Bool
     let canRedeem: Bool
-    let dailyInterestPerThousand: Decimal?
     let featured: Bool
     let minPurchaseAmount: Decimal?
     let productId: String
@@ -20,13 +20,13 @@ public struct SavingProduct: Codable {
     let status: String
     let upLimit: Decimal?
     let upLimitPerUser: Decimal?
+    var tierAnnualInterestRate: [String: Decimal?]?
 
     enum CodingKeys: String, CodingKey {
         case asset
         case avgAnnualInterestRate
         case canPurchase
         case canRedeem
-        case dailyInterestPerThousand
         case featured
         case minPurchaseAmount
         case productId
@@ -34,6 +34,7 @@ public struct SavingProduct: Codable {
         case status
         case upLimit
         case upLimitPerUser
+        case tierAnnualInterestRate
     }
 
     public init(from decoder: Decoder) throws {
@@ -43,7 +44,6 @@ public struct SavingProduct: Codable {
         avgAnnualInterestRate = Decimal(string: try container.decode(String.self, forKey: .avgAnnualInterestRate))
         canPurchase = try container.decode(Bool.self, forKey: .canPurchase)
         canRedeem = try container.decode(Bool.self, forKey: .canRedeem)
-        dailyInterestPerThousand = Decimal(string: try container.decode(String.self, forKey: .dailyInterestPerThousand))
         featured = try container.decode(Bool.self, forKey: .featured)
         minPurchaseAmount = Decimal(string: try container.decode(String.self, forKey: .minPurchaseAmount))
         productId = try container.decode(String.self, forKey: .productId)
@@ -51,5 +51,8 @@ public struct SavingProduct: Codable {
         status = try container.decode(String.self, forKey: .status)
         upLimit = Decimal(string: try container.decode(String.self, forKey: .upLimit))
         upLimitPerUser = Decimal(string: try container.decode(String.self, forKey: .upLimitPerUser))
+        if let stringDict = try? container.decode([String: String].self, forKey: .tierAnnualInterestRate) {
+            tierAnnualInterestRate = Dictionary(uniqueKeysWithValues: stringDict.map { ($0.0, Decimal(string: $0.1)) })
+        }
     }
 }
